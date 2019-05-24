4eb4a16f246efab7c7201c3a066fb342695b2e94
Jules Lagarde <jules.lagarde@gmail.com>
2019-05-24 18:19:35 +0200

    fuse file

--- routes/chatbot.js
+++ routes/chatbot.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-var chatbots = [];
+let cerveaux = [];
+let chatBots = [];
 
 const RiveScript = require("rivescript")
 
@@ -10,24 +11,46 @@ function loading_error(error, filename, lineno) {
 }
 
 for(let i=0; i<2; i++){
-  let bot = new RiveScript({utf8: true});
-  bot.display={id:i, status:"off", cerveau:"steve"};
-  bot.loadDirectory("cerveaux").then(()=>{
-    bot.sortReplies();
-    console.log("bot finished loading")
+  let cerveau = new RiveScript({utf8: true});
+  cerveau.display={id:i, status:"off", cerveau:"steve", url:"http://localhost:"+(1001+i)};
+  cerveau.loadDirectory("cerveaux").then(()=>{
+    cerveau.sortReplies();
+    console.log("cerveau finished loading")
   }).catch(loading_error);
-  chatbots.push(bot);
+  cerveaux.push(cerveau);
 }
+
+
+function createAppBot(name, cerveau){
+
+  const appBot = express();
+
+  appBot.use(express.json());
+
+  appBot.post("/", (req, res)=>{
+    res.send("working")
+  });
+
+  let port =1000+chatBots.length;
+  appBot.listen(port, ()=>{
+    console.log("bot '"+name+"' has started running on port "+port);
+  });
+
+  chatBots.push(appBot);
+  return appBot;
+}
+
 // route for user logout
 router.get('/', (req, res) => {
-  res.send(chatbots)
+  res.send(cerveaux.map((x)=>x.display))
 });
-// route for user logout
+
+// route for user logout //todo move this on each port
 router.get('/:botID/say/:message', (req, res) => {
   req.params["message"]=req.params["message"].replace(/_/g, " ");
   console.log("GET "+JSON.stringify(req.params));
   let id = parseInt(req.params["botID"]);
-  chatbots[id].reply("local", req.params["message"]).then(function(reply) {
+  cerveaux[id].reply("local", req.params["message"]).then(function(reply) {
     res.send(reply);
   });
 });
