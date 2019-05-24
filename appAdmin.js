0a7986195ecc1d0659e974ba303feb9cd824cb72
Jules Lagarde <jules.lagarde@gmail.com>
2019-05-24 17:56:30 +0200

    un bot => un port

---
+++ appAdmin.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const appAdmin = express();
+
+// appAdmin.set('views', path.join(__dirname, 'views'));
+// appAdmin.set('view engine', 'pug');
+
+appAdmin.use(express.json());
+
+const elements = ["cerveau","chatbot"];
+for(route of elements) {
+    appAdmin.use("/rest/"+route, require("./routes/" + route));
+}
+
+appAdmin.use(express.static('public'));
+
+module.exports = appAdmin;
--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const app = require('./app');
+const app = require('./appAdmin');
 
 const server = app.listen(3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
